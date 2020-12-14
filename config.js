790a27712e9120d941926e96def92154a67b7062
Marko Taskovic <marko.taskovic@fazi.rs>
2020-12-14 10:44:17 +0100

    Initial: Start server application

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PORT: process.env.PORT || 4000,
+    DATABASE : {
+        host     : 'localhost',
+        user     : 'marko@marko.com',
+        password : '',
+        database : 'baddevelopers'
+    }
+}
