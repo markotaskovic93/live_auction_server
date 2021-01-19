e79f4a57c2ed7fe3092d114c2ffb8c05042f1b3e
Marko Taskovic <marko.taskovic@fazi.rs>
2021-01-19 11:58:24 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const http = require("http")
-const express = require("express")
-const socketio = require("socket.io")
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
 const io = socketio(server, {
   cors: {
     origin: "*",
   },
-})
+});
 
 io.on("connect", (socket) => {
-  console.log("someone connected")
-  socket.emit("connected") //handle new connection
+  console.log("someone connected");
+  socket.emit("connected");
 
   socket.on("new_message", (messageData) => {
     console.log(messageData)
     io.emit("message", messageData)
-  })
+  });
 
   socket.on("disconnect", () => {
     console.log("user disconnected")
-  })
-})
+  });
+});
 
-server.listen(PORT, () => console.log(`listening to port ${PORT}`))
+server.listen(PORT, () => console.log(`listening to port ${PORT}`));
